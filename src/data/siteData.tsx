821e99687d81a3212e035d55035fc2a740308f7e
Bradley Nguyen <137482087+BradleyNgu@users.noreply.github.com>
2026-08-31 15:32:43 -0400

    intern data

--- src/data/siteData.tsx
+++ src/data/siteData.tsx
@@ -191,7 +191,7 @@ export const experiences: Experience[] = [
   {
     id: '1',
     company: 'Tangerine',
-    position: 'Project Manager',
+    position: 'Project Manager Intern',
     period: 'Sep. 2026 – Dec. 2026',
     location: 'Toronto, ON',
     logo: '/assets/icons/images.jpeg',
@@ -201,7 +201,7 @@ export const experiences: Experience[] = [
   {
     id: '2',
     company: 'Innovation, Science and Economic Development Canada',
-    position: 'Data Engineer',
+    position: 'Data Engineer Intern',
     period: 'May 2026 – Aug. 2026',
     location: 'Ottawa, ON',
     logo: '/assets/icons/ISED.jpg',
@@ -228,7 +228,7 @@ export const experiences: Experience[] = [
   {
     id: '4',
     company: 'dynaCERT Inc.',
-    position: 'Full-Stack Developer',
+    position: 'Full-Stack Developer Intern',
     period: 'May 2025 – Dec. 2025',
     location: 'Toronto, ON',
     logo: '/assets/icons/dynacert.png',
